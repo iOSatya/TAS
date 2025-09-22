c56f577c0f9e1433c4e67fc29b13803e17e9d795
iOSatya <125645326+iOSatya@users.noreply.github.com>
2025-09-22 16:00:00 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+
+  <div style="color: var(--dark);">
+    <Navigator />
+    <RouterView />
+  </div>
+
+
+</template>
+
+<script setup>
+import Navigator from './components/Navigator.vue';
+
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Navigator.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="flex fixed shadow-xl w-screen justify-center z-50" style="background-color: oklch(26.9% 0 0 / 0.5); color: var(--light);">
+    <nav class="flex">
+      <RouterLink :to="{ name: 'home' }">Home</RouterLink>
+      <RouterLink :to="{ name: 'about' }">About Us</RouterLink>
+      <RouterLink :to="{ name: 'contact' }">Contact Us</RouterLink>
+    </nav>
+  </div>
+
+</template>
+
+<style scoped>
+
+  a {
+    padding: 1rem 2rem;
+  }
+
+  /* a:hover {
+    color: var(--secondary);
+  } */
+
+  a.router-link-active {
+    border-bottom: 2px var(--light) solid;
+  }
+
+</style>
